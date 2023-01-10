b3691854dca8dace08a1876fa89158327358b194
Mehtap Tataroğlu <teytap@gmail.com>
2023-01-10 21:44:39 +0300

    rewrote function  with async await

--- src/app.js
+++ src/app.js
@@ -3,14 +3,23 @@ const quote = document.getElementById("quote");
 const adviceId = document.getElementById("advice-id");
 const apiUrl = `https://api.adviceslip.com/advice`;
 
-function getQuote(response) {
-  quote.innerHTML = `"${response.data.slip.advice}"`;
-  adviceId.innerHTML = response.data.slip.id;
-}
+// function getQuote(response) {
+//   quote.innerHTML = `"${response.data.slip.advice}"`;
+//   adviceId.innerHTML = response.data.slip.id;
+// }
 
-function newQuote(event) {
-  event.preventDefault();
-  axios.get(apiUrl).then(getQuote);
+// function newQuote(event) {
+//   event.preventDefault();
+//   axios.get(apiUrl).then(getQuote);
+// }
+// btn.addEventListener("click", newQuote);
+// axios.get(apiUrl).then(getQuote);
+async function getQuote() {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  quote.innerHTML = `"${data.slip.advice}"`;
+  adviceId.innerHTML = data.slip.id;
 }
-btn.addEventListener("click", newQuote);
-axios.get(apiUrl).then(getQuote);
+getQuote();
+
+btn.addEventListener("click", getQuote);
